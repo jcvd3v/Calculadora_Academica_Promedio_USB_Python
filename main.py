0f69c07ae5703127a5703b6067c1de85e31718a8
jcva2002 <jcv20092002@gmail.com>
2026-01-20 17:57:52 -0400

    corrección main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-# Sistema de Calificaciones USB. Sumar calificaciones USB y Creditos Inscritos. Calcular el promedio actual. Calcular Notas Minimas Requeridas para mantener el promedio. Identifica Estado de PP.
-# Implementar dataclass en futuras versiones.
-# Agregar Interfaz Grafica, Ingreso De Datos, Registro Academico del Estudiante, USBID, Base de Datos para ese Usuario en base a su USBID, Estadisticas de Materias Fuertes Y debiles para recomendar en cuales debe enfocarse.
-# Integrar base de datos
-# Crear tabla en base de datos con el titulo de la id del usuario en lugar de usuario, así aseguro que haya solo una por cada usuario.
-# Si es la primera vez que el usuario abre el programa (no existe base de datos) que le pida ingresar materias.
+# Sistema de Calificaciones USB. Sumar calificaciones USB y Creditos Inscritos. Calcular el promedio actual.  Identifica Estado de PP.
+# Implementar dataclass en futuras versiones:
+# Agregar Interfaz Grafica, Estadisticas de Materias Fuertes Y debiles para recomendar en cuales debe enfocarse, Calcular Notas Minimas Requeridas para mantener el promedio.
+# Si es la primera vez que el usuario abre el programa (no existe archivo csv) que le pida ingresar materias.
 
 # ============== Modelos ============= #
 from src.models.materia import Materia
