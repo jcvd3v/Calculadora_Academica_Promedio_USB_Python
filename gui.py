88466343b6f130941a13779757fa0c8bc1b5e4c8
jcva2002 <jcv20092002@gmail.com>
2026-01-23 18:58:51 -0400

    feat(gui):row error fixed, subject scrollable table

--- gui.py
+++ gui.py
@@ -12,25 +12,34 @@ app.geometry("600x400")
 # Grid settings. 2 Columns: Subjects/Buttons
 app.grid_columnconfigure(0, weight=3)  # Subjects Column
 app.grid_columnconfigure(1, weight=1)  # Buttons Column
-app.grid_rowconfigure(0,1)
+app.grid_rowconfigure(0,weight=1)
 
 # Table
-frame = ctk.CTkFrame (
+frame_table = ctk.CTkScrollableFrame (
     app,
     border_width=2,
     border_color="gray",
-    corner_radius=20
+    corner_radius=20,
+    label_text="Materias"
 )
-frame.pack(pady=20, padx=10, fill="both", expand=True)
+
+frame_table.grid (
+    row=0, 
+    column=0, 
+    padx=20, 
+    pady=20, 
+    sticky="nsew")
+
+frame_table.pack()
 
 # Buttons
-btn_add = ctk.CTkButton(frame, text="Agregar Materia")
+btn_add = ctk.CTkButton(app, text="Agregar Materia")
 btn_add.pack()
-btn_delete = ctk.CTkButton(frame, text="Eliminar Materia")
+btn_delete = ctk.CTkButton(app, text="Eliminar Materia")
 btn_delete.pack()
-btn_modify = ctk.CTkButton(frame, text="Modificar Materia")
+btn_modify = ctk.CTkButton(app, text="Modificar Materia")
 btn_modify.pack()
-btn_stats = ctk.CTkButton(frame, text="Ver Estadisticas")
+btn_stats = ctk.CTkButton(app, text="Ver Estadisticas")
 btn_stats.pack()
 
 # Run
